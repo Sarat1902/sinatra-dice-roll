3ebd66dd03d9aa38e063cf4afd17680f87c24a40
Sarat1902 <161099567+Sarat1902@users.noreply.github.com>
2024-04-05 23:16:04 +0000

    commit

--- dice.rb
+++ dice.rb
@@ -17,25 +17,24 @@ get("/dice/2/6") do
   second_die = rand(1..6)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  erb(:two_six)
+
 end
 
 get("/dice/2/10") do
   first_die = rand(1..10)
   second_die = rand(1..10)
   sum = first_die + second_die
-  outcome = "you rolled a #{first_die} and a #{second_die} for a total of #{sum} "
+  @outcome = "you rolled a #{first_die} and a #{second_die} for a total of #{sum} "
 
-  "<h1>2d10</h1>
-  <p>#{outcome}</p>"
+  erb(:two_ten)
 end
 
 get("/dice/1/20") do
   die = rand(1..20)
-  outcome = "you rolled a #{die}"
+  @outcome = "you rolled a #{die}"
 
   "<h1>1d20</h1>
   <p>#{outcome}</p>"
@@ -52,7 +51,7 @@ get("/dice/5/4") do
       i = i + 1
     end
   
-    outcome = "Your roll on all the 5 dice #{dice} for a total of #{sum}"
+    @outcome = "Your roll on all the 5 dice #{dice} for a total of #{sum}"
     "<h1>5d4</h1>
     <p>#{outcome}</p>"
   end
