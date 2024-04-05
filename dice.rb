21f1c5dbb44816f103bc9ced0c8ce3e5225c3b44
Sarat1902 <161099567+Sarat1902@users.noreply.github.com>
2024-04-05 21:48:08 +0000

    commit

--- dice.rb
+++ dice.rb
@@ -1,6 +1,15 @@
 require "sinatra"
+require "sinatra/reloader"
+
+get("/") do
+  "Hello World"
+end
 
 
-get "/zebra" do
+get("/zebra") do
   "we must add a route"
+end
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
 end
